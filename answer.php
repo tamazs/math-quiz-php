7afe3c6c00e5d12cbea39b633f2b056d2126d96f
tamazs <majortamas313@gmail.com>
2021-08-21 18:26:45 +0200

    test

---
+++ answer.php
@@ -0,0 +1,13 @@
+<?php
+
+    if(isset($_POST['check'])) {
+        $min = 1;
+        $max = 100;
+        $random = rand($min,$max);
+        $number1 = $_POST['number1'];
+        $number2 = $_POST['number2'];
+        $number3 = $_POST['number3'];
+
+
+    };
+?>
