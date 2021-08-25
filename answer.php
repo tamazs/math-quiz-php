60ea8e795642f707bb8a1a820e2e548f0ed2a1e3
tamazs <majortamas313@gmail.com>
2021-08-25 17:28:54 +0200

    fix

--- answer.php
+++ answer.php
@@ -1,12 +1,13 @@
 <?php
     $number1 = $_POST["number1"];
     $number2 = $_POST["number2"];
+    $sum = $number1 + $number2;
 
     $result = $_POST["result"];
 
-    if ($number1 + $number2 == $result) {
+    if ($sum == $result) {
         echo "Your answer is right!";
     } else {
-        echo "Your answer is wrong! The right answer is: $result.";
+        echo "Your answer is wrong! The right answer is: $sum.";
     }
 ?>
